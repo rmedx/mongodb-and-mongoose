dc4534d5cf67c369d6d6deb8cc60b4fe1bbee208
rmedx <65210483+rmedx@users.noreply.github.com>
2021-08-19 20:09:29 -0400

    completed findAndUpdate

--- myApp.js
+++ myApp.js
@@ -82,8 +82,12 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}).exec((err, data) => {
+    if (err) {
+      return done(err);
+    }
+    done(null, data);
+  })
 };
 
 const removeById = (personId, done) => {
